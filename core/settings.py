b85117d976572beb70369261edba636237a6d8e2
Natzsmart <iniobongeffiong40@gmail.com>
2023-03-03 20:09:50 +0100

    Made changes and installed dj database url

--- core/settings.py
+++ core/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-sw1h_oz21hm(33=b(j7@34ko7ogh&)$76@k8gsa00o$1l%$123'
+SECRET_KEY = os.environ.get("SECRET_KEY", "1773830202hfd")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'RENDER' not in os.environ
@@ -144,7 +144,7 @@ CORS_ORIGIN_ALLOW_ALL= True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR / "static"),
